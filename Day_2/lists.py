5ac181bfd9111a46a3ad71ba3014ee68a6903603
Ram_S <kasiramaraos@gmail.com>
2026-06-06 15:08:02 -0500

    just added another list to the lists file and performed remove, pop, adding an item at a particular index using list_name[i] and also the length finally

--- Day_2/lists.py
+++ Day_2/lists.py
@@ -5,3 +5,11 @@ fav.append("Gym")
 print(len(fav))
 for i in fav:
     print(i)
+
+exis=["Limitation","Facticity","expectation","ocd","bond"]
+exis[1]="love"
+print(exis[::1])
+exis.remove("love")
+exis.pop()
+print(exis[::1])
+print(len(exis))
