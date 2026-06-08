0c76568f650422fc9ebc923a48244fd803768c45
Ram_S <kasiramaraos@gmail.com>
2026-06-08 11:37:14 -0500

    created a dictionary with my general details and printed by calling the key name and also changes the value  and printed all the values using for loop

---
+++ Day_4/dict.py
@@ -0,0 +1,6 @@
+Details={"name":"Kasi Ram" , "Age":24, "university":"UTA" ,"goal":"Make parents happy and comfortable by giving all needs"}
+print(Details["name"])
+print(Details["Age"])
+Details["name"]="Kasi Rama Rao"
+for i in Details:
+    print(Details[i])
